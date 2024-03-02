910c6c0564b0edbc713fbd8abb3d411672153cbb
mdnestor <mdnest01@gmail.com>
2024-03-02 15:56:52 -0700

    Update cellular_automata.lean

--- scripts/cellular_automata.lean
+++ scripts/cellular_automata.lean
@@ -1,8 +1,6 @@
-/-
-inspired by "Cellular Automata and Groups" by Tullio Ceccherini-Silberstein and Michel Coornaert (https://link.springer.com/book/10.1007/978-3-642-14034-1).
-what are gliders in a Cellular Automata like Game of Life?
-first off they are field configurations, meaning we need a domain and codomain
--/
+
+/- what are gliders in a Cellular Automata like Game of Life? -/
+/- first off they are field configurations, meaning we need a domain and codomain -/
 
 def subset (X: Type): Type := X -> Prop
 
@@ -14,10 +12,8 @@ def neighborhood_of_influence {X Y: Type} (F: (X -> Y) -> (X -> Y)) (x: X): subs
 
 def local_rule {X Y: Type} (F: (X -> Y) -> (X -> Y)): Prop := forall x: X, compact (neighborhood_of_influence F x)
 
-/-
-a field configuration is compact if it's nonzero on a compact set
-this means we need a notion of zero in the target space
--/
+/- a field configuration is compact if it's nonzero on a compact set
+this means we need a notion of zero in the target space -/
 
 def support {X Y: Type} (u: X -> Y) (y0: Y): subset X := sorry
 
@@ -25,9 +21,7 @@ def ucompact {X Y: Type} (u: X -> Y) (y0: Y): Prop := compact (support u y0)
 
 theorem local_rule_preserves_compact {X Y: Type} (F: (X -> Y) -> (X -> Y)) (u: X -> Y) (y0: Y) (h1: local_rule F) (h2: ucompact u y0): ucompact (F u) y0 := sorry
 
-/-
-now I need translation rules
--/
+/- now I need translation rules -/
 
 structure Group (T: Type) where
   op: T -> T -> T
@@ -85,13 +79,61 @@ def extended_action {Z X: Type} {G: Group Z} (A: GroupAction G X) (Y: Type): Gro
 def GliderSpace {X Y Z: Type} {G: Group Z} (A: GroupAction G X) (F: (X -> Y) -> (X -> Y)) (u: X -> Y) (y0: Y) (h: glider F u y0): subset (X -> Y) :=
   smallest_containing_invariant_subset (singleton u) (extended_action A Y)
 
-/-
-Next I guess we want to look at like
-decomposing u: X -> Y into connected components
+/- Next I guess we want to look at like -/
+/- decomposing u: X -> Y into connected components -/
 
-we also want to look at "backwards" solutions composed of gliders as t -> -infinity
+/- we also want to look at "backwards" solutions composed of gliders as t -> -infinity -/
 
-want to prove the curtis theorem
+/- want to prove the curtis theorem -/
+/- as well as my maybe original theorem, "existence at t=T and uniform random initial data implies ubiquity at t=T" -/
 
-as well as my maybe original theorem, "existence at t=T and uniform random initial data implies ubiquity at t=T"
--/
+/- definition 1.4.1 -/
+
+structure Group2 where
+  T: Type
+  op: T -> T -> T
+  id: T
+  inv: T -> T
+
+structure subtype {X: Type} (S: subset X) where
+  x: X
+  belongs: S x
+
+def finite {X: Type} (S: subset X): Prop := sorry
+
+def restrict_to_subtype {X Y: Type} (f: X -> Y) (S: subset X): subtype S -> Y := fun x => f x.x
+
+def lmul {A: Type} {G: Group2} (g0: G.T) (x: G.T -> A): G.T -> A := fun g => x (G.op (G.inv g0) g)
+
+/- some sort of composition -/
+
+def cellular_automaton {A: Type} {G: Group2} (F: (G.T -> A) -> (G.T -> A)): Prop := exists S: subset G.T, finite S ∧ exists mu: (subtype S -> A) -> A, forall x: G.T -> A, forall g: G.T, (F x) g = mu (restrict_to_subtype x S)
+
+/-  we need the prodiscrete topology -/
+
+structure TopologicalSpace (X: Type) where
+
+/- suppose we have two types X and Y and for every y a topological space Tx on X -/
+
+def DiscreteTopology (X: Type): TopologicalSpace X := sorry
+
+def FFF {X Y: Type} (f: Y -> TopologicalSpace X): TopologicalSpace (X -> Y) := {
+
+}
+
+def ProdiscreteTopology (X Y: Type): TopologicalSpace (X -> Y) := FFF (fun _ => DiscreteTopology X)
+
+
+structure TopSpace where
+
+
+def is_prodiscrete_continuous (f: (X -> Y) -> (X -> Y)): Prop := sorry
+
+def Prodiscrete (A: Type) (G: Group2): TopSpace := sorry
+
+def equivariant2 {A: Type} {G: Group2} (tau: (G.T -> A) -> (G.T -> A)): Prop := forall x: G.T -> A, forall g: G.T, tau (lmul g x) = lmul g (tau x)
+
+def finiteType (X: Type): Prop := sorry
+
+theorem CurtisHedlundLyndon (A: Type) (G: Group2) (h: finiteType A) (tau: (G.T -> A) -> (G.T -> A)):
+  (cellular_automaton tau) ↔ (is_prodiscrete_continuous tau) ∧ (equivariant2 tau) := sorry
