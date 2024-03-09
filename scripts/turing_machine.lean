8241fe235c0619f3297758dd6f3cb600e01bd261
mdnestor <mdnest01@gmail.com>
2024-03-08 20:17:32 -0700

    Update turing_machine.lean

--- scripts/turing_machine.lean
+++ scripts/turing_machine.lean
@@ -10,7 +10,7 @@ structure DiscreteDynamicalSystem where
   state: Type
   update: state → state
 
-example (M: TuringMachine) [DecidableEq M.tape]: DiscreteDynamicalSystem := {
+def F (M: TuringMachine) (h: DecidableEq M.tape): DiscreteDynamicalSystem := {
   state := (M.tape → M.symbol) × M.state × M.tape
   update := fun (u, s, x) => by
     have (y1, y2, y3) := M.update (s, (u x))
@@ -21,4 +21,32 @@ example (M: TuringMachine) [DecidableEq M.tape]: DiscreteDynamicalSystem := {
     )
 }
 
-/- some theorem to cap it off? -/
+/-
+some theorem to cap it off?
+that there is a turing machine U
+such that for any other given turing machine M
+you can encode into an input of U
+and simulate it in U
+-/
+
+structure IOMachine where
+  machine: TuringMachine
+  h: DecidableEq machine.tape
+  input: Type
+  output: Type
+  encode: input → (F machine h).state
+  decode: (F machine h).state → output
+
+def run (S: DiscreteDynamicalSystem) (x0: S.state): Nat → S.state :=
+  fun n => match n with
+  | Nat.zero => x0
+  | Nat.succ n_previous => S.update ((run S x0) n_previous)
+
+def reaches (S: DiscreteDynamicalSystem) (x0 x1: S.state): Prop :=
+  ∃ n: Nat, (run S x0) n = x1
+
+def halts_on_input (M: IOMachine) (h: DecidableEq M.machine.tape) (s0: M.machine.state) (i: M.input): Prop :=
+  ∃ n: Nat, ((run (F M.machine h) (M.encode i)) n).2.1 = s0
+
+def halts (M: IOMachine) (h: DecidableEq M.machine.tape) (s_halt: M.machine.state): Prop :=
+  ∀ i: M.input, halts_on_input M h s_halt i
