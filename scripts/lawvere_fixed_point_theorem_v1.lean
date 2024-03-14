83d9e5d266521457e6196bae5545d305ef8b2e2f
mdnestor <mdnest01@gmail.com>
2024-03-14 17:43:34 -0600

    Update lawvere_fixed_point_theorem_v1.lean

--- scripts/lawvere_fixed_point_theorem_v1.lean
+++ scripts/lawvere_fixed_point_theorem_v1.lean
@@ -11,11 +11,11 @@ def weakly_point_surjective (g: X → A → Y): Prop :=
 def fixed_point_property (Y: Type): Prop :=
   ∀ t: Y → Y, ∃ y: Y, t y = y
 
-theorem Lawvere: ∀ Y: Type, (∃ A: Type, ∃ g: A → A → Y, weakly_point_surjective g) → fixed_point_property Y := by
+theorem Lawvere: ∀ Y: Type, (∃ g: A → A → Y, weakly_point_surjective g) → fixed_point_property Y := by
   intro Y h1
   rw [fixed_point_property]
   intro t
-  obtain ⟨A, g, h2⟩ := h1
+  obtain ⟨g, h2⟩ := h1
   obtain ⟨f, h3⟩: ∃ f: A → Y, ∀ a: A, f a = t (g a a) := ⟨fun a => t (g a a), by simp⟩
   obtain ⟨x, h4⟩ := h2 f
   exists g x x
