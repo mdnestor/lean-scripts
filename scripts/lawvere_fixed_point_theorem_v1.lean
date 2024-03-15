bcdf16e9527074e0426de2690a50229ce81b1cfb
mdnestor <mdnest01@gmail.com>
2024-03-14 21:30:51 -0600

    Update lawvere_fixed_point_theorem_v1.lean

--- scripts/lawvere_fixed_point_theorem_v1.lean
+++ scripts/lawvere_fixed_point_theorem_v1.lean
@@ -4,8 +4,7 @@ http://tac.mta.ca/tac/reprints/articles/15/tr15.pdf
 A simple version where we identify A^B with A → B.
 Basically a generalized Cantor's theorem for types.
 -/
-
-def weakly_point_surjective (g: X → A → Y): Prop :=
+def weakly_point_surjective {A X Y: Type} (g: X → A → Y): Prop :=
   ∀ f: A → Y, ∃ x: X, ∀ a: A, (g x) a = f a
 
 def fixed_point_property (Y: Type): Prop :=
@@ -22,13 +21,11 @@ theorem Lawvere: ∀ Y: Type, (∃ g: A → A → Y, weakly_point_surjective g) 
   rw [←h3, h4]
 
 /-
-Generalizations?
-Yanofsky (2003) "A universal approach to self-referential paradoxes, incompleteness and fixed points" https://arxiv.org/pdf/math/0305282.pdf
-Roberts (2023) "Substructural fixed-point theorems and the diagonal argument: theme and variations" https://compositionality-journal.org/papers/compositionality-5-8/pdf/
+Generalization due to Yanofsky (2003): "A universal approach to self-referential paradoxes, incompleteness and fixed points" https://arxiv.org/pdf/math/0305282.pdf
 -/
 
 def representable {A Y: Type} (g: A → A → Y) (f: A → Y): Prop :=
-  ∃ a: A, f = fun a' => g a a'
+  ∃ a: A, ∀ a': A, f a' = g a a'
 
 def represents_all {A Y: Type} (g: A → A → Y): Prop :=
   ∀ f: A → Y, representable g f
@@ -36,12 +33,17 @@ def represents_all {A Y: Type} (g: A → A → Y): Prop :=
 def represents_some {A Y: Type} (g: A → A → Y): Prop :=
   ∀ t: Y → Y, representable g (fun a => t (g a a))
 
+/-
+The following lemmas show "represents all" is a weaker condition than weakly point surjective.
+-/
+
 def wps_implies_ra {A Y: Type} (g: A → A → Y): weakly_point_surjective g → represents_all g := by
   intro h1 f
   rw [representable]
   obtain ⟨x, h2⟩ := h1 f
   exists x
   funext
+  intro a
   apply Eq.symm
   apply h2
 
@@ -50,17 +52,24 @@ def ra_implies_rs {A Y: Type} (g: A → A → Y): represents_all g → represent
   apply h1
 
 def wps_implies_rs {A Y: Type} (g: A → A → Y): weakly_point_surjective g → represents_some g := by
-  intros
+  intro h
   apply ra_implies_rs
   apply wps_implies_ra
+  exact h
   
 theorem Yanofsky: ∀ Y: Type, (∃ g: A → A → Y, represents_some g) → fixed_point_property Y := by
   intro Y h1
   rw [fixed_point_property]
   intro t
   obtain ⟨g, h2⟩ := h1
-  obtain ⟨f, h3⟩: ∃ f: A → Y, ∀ a: A, f a = t (g a a) := ⟨fun a => t (g a a), by simp⟩
-  obtain ⟨x, h4⟩ :=  h2 t
-  have h5 : ∀ a: A, t (g a a) = g x a := sorry
+  obtain ⟨x, h4⟩ := h2 t
   exists g x x
-  apply h5
+  apply h4
+
+/- Proving Lawvere's theorem using Yanofsky's -/
+theorem LawvereProof2: ∀ Y: Type, (∃ g: A → A → Y, weakly_point_surjective g) → fixed_point_property Y := by
+  intro Y h1
+  obtain ⟨g, h2⟩ := h1
+  have h3: represents_some g := (wps_implies_rs g) h2
+  apply Yanofsky
+  exists g
