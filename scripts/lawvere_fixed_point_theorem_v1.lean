e992b06ffca2cc7a6227dfeec737af090bfabb5c
mdnestor <mdnest01@gmail.com>
2024-03-14 21:09:18 -0600

    Update lawvere_fixed_point_theorem_v1.lean

--- scripts/lawvere_fixed_point_theorem_v1.lean
+++ scripts/lawvere_fixed_point_theorem_v1.lean
@@ -20,3 +20,47 @@ theorem Lawvere: ∀ Y: Type, (∃ g: A → A → Y, weakly_point_surjective g) 
   obtain ⟨x, h4⟩ := h2 f
   exists g x x
   rw [←h3, h4]
+
+/-
+Generalizations?
+Yanofsky (2003) "A universal approach to self-referential paradoxes, incompleteness and fixed points" https://arxiv.org/pdf/math/0305282.pdf
+Roberts (2023) "Substructural fixed-point theorems and the diagonal argument: theme and variations" https://compositionality-journal.org/papers/compositionality-5-8/pdf/
+-/
+
+def representable {A Y: Type} (g: A → A → Y) (f: A → Y): Prop :=
+  ∃ a: A, f = fun a' => g a a'
+
+def represents_all {A Y: Type} (g: A → A → Y): Prop :=
+  ∀ f: A → Y, representable g f
+
+def represents_some {A Y: Type} (g: A → A → Y): Prop :=
+  ∀ t: Y → Y, representable g (fun a => t (g a a))
+
+def wps_implies_ra {A Y: Type} (g: A → A → Y): weakly_point_surjective g → represents_all g := by
+  intro h1 f
+  rw [representable]
+  obtain ⟨x, h2⟩ := h1 f
+  exists x
+  funext
+  apply Eq.symm
+  apply h2
+
+def ra_implies_rs {A Y: Type} (g: A → A → Y): represents_all g → represents_some g := by
+  intro h1 _
+  apply h1
+
+def wps_implies_rs {A Y: Type} (g: A → A → Y): weakly_point_surjective g → represents_some g := by
+  intros
+  apply ra_implies_rs
+  apply wps_implies_ra
+  
+theorem Yanofsky: ∀ Y: Type, (∃ g: A → A → Y, represents_some g) → fixed_point_property Y := by
+  intro Y h1
+  rw [fixed_point_property]
+  intro t
+  obtain ⟨g, h2⟩ := h1
+  obtain ⟨f, h3⟩: ∃ f: A → Y, ∀ a: A, f a = t (g a a) := ⟨fun a => t (g a a), by simp⟩
+  obtain ⟨x, h4⟩ :=  h2 t
+  have h5 : ∀ a: A, t (g a a) = g x a := sorry
+  exists g x x
+  apply h5
