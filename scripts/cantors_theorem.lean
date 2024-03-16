dcf31c9c4c53a8a5c4bd6dfb2a78f1b1fbec3082
mdnestor <mdnest01@gmail.com>
2024-03-16 09:34:14 -0600

    Update cantors_theorem.lean

--- scripts/cantors_theorem.lean
+++ scripts/cantors_theorem.lean
@@ -1,40 +1,28 @@
 /-
-Cantor's theorem: Let A be a set, and let P(A) be its power set. Then every function f: A → P(A) is non-surjective.
-Proof: Suppose for a contradiction f is surjective. Define B = {x in A | x ∉ f(x)} Then ∃ z ∈ A such that f(z) = B. This yields the contradiction z ∈ B ↔ z ∉ f(z) = B.
+Cantor's theorem:
+Let A be a set, and let P(A) be its power set.
+Then every function f: A → P(A) is non-surjective.
+
+Proof:
+Suppose for a contradiction f is surjective.
+Define B = {x in A | x ∉ f(x)}.
+Then ∃ z ∈ A such that f(z) = B.
+This yields the contradiction z ∈ B ↔ z ∉ f(z) = B.
 -/
 
 def subset (A: Type): Type := A → Prop
 
 def surjective {A B: Type} (f: A → B): Prop := ∀ b: B, ∃ a: A, f a = b
 
-axiom contradiction {P: Prop} (h: ¬ P → False): P
-
-def B (f: A → subset A): subset A := fun x => ¬ (f x) x
- 
-def prop2 (f: A → subset A) (h: surjective f): ∃ z: A, f z = B f := sorry
-
-def prop3 (f: A → subset A) (h: surjective f) (z: A) (hz: f z = B f): (B f) z → ¬ (B f) z := sorry
-
-def prop4 (f: A → subset A) (x: A): (B f) x → ¬ (f x) x := sorry
-
-def prop5 (f: A → subset A) (x: A): ¬ (B f) x → (f x) x := sorry
-
-def prop6 {P: Prop} (h: P → ¬ P): ¬ P := sorry
-
-def prop7 (f: A → subset A) (x: A) (hx: f x = B f): (B f) x → ¬ (B f) x := sorry
-
-def prop8 (f: A → subset A) (x: A) (hx: f x = B f): ¬ (B f) x := sorry
-
-def prop9 (f: A → subset A) (x: A) (hx: f x = B f): ¬ (B f) x → (B f) x := sorry
-
-def prop10 (f: A → subset A) (x: A) (hx: f x = B f): (B f) x := sorry
-
-def prop11 (f: A → subset A) (x: A) (hx: f x = B f): False := sorry
-
 theorem Cantor: ∀ A: Type, ∀ f: A → subset A, ¬ surjective f := by
   intro A f
-  have z: A := sorry
-  have hz: f z = B f := sorry
-  have hc: surjective f → False := sorry /- use i -/
-  have hc2: ¬¬surjective f → False := sorry
-  exact contradiction hc2
+  apply Not.intro
+  intro h1
+  obtain ⟨B, h2⟩: ∃ B: subset A, ∀ x: A, (B x → ¬ (f x) x) ∧ (¬ (f x) x → B x) := by
+    exists fun x => ¬ (f x) x
+    simp 
+  obtain ⟨z, h3⟩ := h1 B
+  have h4 := h2 z
+  rw [h3] at h4
+  have h5 := fun x => h4.1 x x
+  exact h5 (h4.2 h5)
