84aa91698b098bdff2cf75e1c3dffbe9f66009be
Michael Nestor <mdnest01@gmail.com>
2024-06-19 12:36:51 -0400

    Update triangular_number.lean

--- scripts/triangular_number.lean
+++ scripts/triangular_number.lean
@@ -1,39 +1,67 @@
 -- baby's first inductive proof: 1+2+3+...+n = n*(n+1)/2
 -- https://en.wikipedia.org/wiki/Triangular_number
 
+-- natural numbers are the quintessential inductive type
+-- https://lean-lang.org/theorem_proving_in_lean4/inductive_types.html
 inductive Natural where
-| first: Natural
+| zero: Natural
 | next: Natural → Natural
-
-axiom P1: ∀ a b: Natural, Natural.next a = Natural.next b → a = b
-axiom P2: ∀ a: Natural, Natural.next a ≠ Natural.first
 
 def add (a b: Natural): Natural :=
   match a with
-  | Natural.first => b
-  | Natural.next a_previous => Natural.next (add a_previous b)
+  -- by cases: if a=0, then a+b = 0+b = b
+  | Natural.zero => b
+  -- otherwise, if a = n+1, then a+b = (n+1)+b = (n+b)+1
+  | Natural.next n => Natural.next (add n b)
 
 def mul (a b: Natural): Natural :=
   match a with
-  | Natural.first => Natural.first
-  | Natural.next a_previous => add (mul a_previous b) b
+  -- again by cases: if a=0, then a*b = 0
+  | Natural.zero => Natural.zero
+  -- otherwise, if a = n+1, then a * b = (n+1) * b = n*b + b
+  | Natural.next n => add (mul n b) b
 
+-- some basic helper theorems, not worth proving here
+theorem add_comm (a b: Natural): add a b = add b a := sorry
+theorem mul_comm (a b: Natural): mul a b = mul b a := sorry
+theorem add_assoc (a b c: Natural): add a (add b c) = add (add a b) c := sorry
+theorem mul_assoc (a b c: Natural): mul a (mul b c) = mul (mul a b) c := sorry
+theorem distrib (a b c: Natural): mul a (add b c) = add (mul a b) (mul a c) := sorry
+
+-- define the n-th triangular number to be 0+1+...+n
 def triangular (n: Natural): Natural :=
   match n with
-  | Natural.first => Natural.first
-  | Natural.next n_previous => add (triangular n_previous) n
+  -- the "zeroth" triangular number is zero
+  | Natural.zero => Natural.zero
+  -- if n = m+1, then the n-th triangular number is the m-th triangular number + n
+  | Natural.next m => add (triangular m) n
 
-def zero: Natural := Natural.first
-def one: Natural := Natural.next Natural.first
+-- aliases
+def one: Natural := Natural.next Natural.zero
 def two: Natural := Natural.next one
 
+-- add local theorems to the simplifier
+attribute [local simp] add mul add_comm mul_comm add_assoc mul_assoc distrib
 
+-- to avoid defining (possibly non-integer) division, the theorem is stated as 2*(1+2+3+...+n) = n*(n+1)
 theorem sum_of_first_n: ∀ n: Natural, mul two (triangular n) = mul n (Natural.next n) := by
   intro n
+  -- proof by induction
   induction n with
-  | first => {
-    rw [triangular, mul, two, one, mul, mul, mul, add, add] -- ez
+  -- base case is simple
+  | zero => {
+    rw [triangular]
+    simp
   }
-  | next n_previous h => {
-    sorry -- damnit this is hard.. think i need distributive property xd
+  -- the inductive step with hyp = the inductive hypothesis on n
+  | next n hyp => {
+    rw [triangular, distrib, hyp]
+    -- once the inductive hypothesis is applied successfully, brute force tree search :D
+    simp
+    rw [mul_comm, mul]
+    have h1: add n n.next = add n.next n := by apply add_comm
+    rw [h1, add]
+    have h2: mul n n.next.next = mul n.next.next n := by apply mul_comm
+    rw [h2, mul, mul]
+    rw [add_comm, add, add, add_comm, add_assoc, add_comm]
   }
