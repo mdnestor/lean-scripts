b7dcdaba923da8194af6e8f9f5a1dac2a2fbacff
mdnestor <mdnest01@gmail.com>
2024-08-05 18:12:07 -0400

    Update contraction_mapping_banach_fixed_point_v3.lean

--- scripts/contraction_mapping_banach_fixed_point_v3.lean
+++ scripts/contraction_mapping_banach_fixed_point_v3.lean
@@ -19,13 +19,35 @@ def Contraction [Dist X] (T: X → X): Prop :=
   ∃ c: Real, 0 ≤ c ∧ c < 1 ∧ ∀ x y: X, dist (T x) (T y) ≤ c * dist x y
 
 theorem contraction_continuous [MetricSpace X] {T: X → X} (h: Contraction T): Continuous T := by
-  sorry
+  rw [Metric.continuous_iff]
+  obtain ⟨c, ⟨c_nonneg, _, contr_ineq⟩⟩ := h
+  intro x ε ε_pos
+  by_cases c_case: c = 0
+  -- case 1: c = 0
+  exists ε
+  apply And.intro
+  exact ε_pos
+  intro y _
+  simp [c_case] at contr_ineq
+  rw [contr_ineq x y, dist_self]
+  exact ε_pos
+  -- case 2: c ≠ 0
+  have c_pos: c > 0 := lt_of_le_of_ne c_nonneg (ne_comm.mp c_case)
+  exists ε/c
+  apply And.intro
+  exact div_pos_iff.mpr (Or.inl ⟨ε_pos, c_pos⟩)
+  intro y x_y_within_eps_div_c
+  calc
+    dist (T y) (T x) ≤ c * dist y x := by apply contr_ineq
+                   _ < c * (ε / c)  := (mul_lt_mul_left c_pos).mpr x_y_within_eps_div_c
+                   _ = ε            := by rw [mul_div_cancel₀ ε c_case]
+
 
 def converge [TopologicalSpace X] (a: Nat → X) (x: X): Prop :=
   Filter.Tendsto a Filter.atTop (nhds x)
 
-def limit_unique [TopologicalSpace X] {a: Nat → X} {x y: X} (h1: converge a x) (h2: converge a y): x = y := by
-  sorry
+def limit_unique {X: Type} [TopologicalSpace X] [T2Space X] {a: Nat → X} {x y: X} (h1: converge a x) (h2: converge a y): x = y := by
+  exact tendsto_nhds_unique h1 h2
 
 theorem tail_converge [TopologicalSpace X] {a: Nat → X} {x: X} (h: converge a x) (t: Nat): converge (fun n => a (n + t)) x := by
   sorry
@@ -35,9 +57,14 @@ theorem apply_continuous_function_to_convergent_sequence [TopologicalSpace X1] [
 
 theorem IterConvergeToFixedPoint {X: Type} {T: X → X} {x x0: X} [MetricSpace X] [CompleteSpace X] (h1: Continuous T) (h2: converge (fun n: Nat => T^[n] x0) x): T x = x := by
   have h3 := apply_continuous_function_to_convergent_sequence h2 h1
-  have h4: converge (fun n => T^[n+1] x0) (T x) := sorry
+  have h4: T ∘ (fun n: Nat => T^[n] x0) = (fun n: Nat => T^[n+1] x0) := by
+    ext n
+    simp
+    rw [Function.Commute.iterate_left]
+    rfl
+  rw [h4] at h3
   have h5 := tail_converge h2 1
-  exact limit_unique h4 h5
+  exact limit_unique h3 h5
 
 theorem iter_cauchy_lemma1 {X: Type} {T: X → X} [MetricSpace X] {c: Real} (c_nonneg: c ≥ 0) (contr_ineq: ∀ (x y : X), dist (T x) (T y) ≤ c * dist x y): ∀ x y: X, ∀ n: Nat, dist (T^[n] x) (T^[n] y) ≤ c^n * dist x y := by
   intro x y n
