9b43245645d1f0b6b0970446cbacf35a243a946a
mdnestor <mdnest01@gmail.com>
2024-09-26 17:55:40 -0400

    Update kan_extension_commute.lean

--- MiscLean/kan_extension_commute.lean
+++ MiscLean/kan_extension_commute.lean
@@ -3,23 +3,25 @@ import Mathlib.CategoryTheory.Functor.KanExtension.Adjunction
 
 open CategoryTheory
 
+universe u v
+
 -- category of indexed sets
 class ISet where
-  X: Type u1
-  A: X → Type u2
+  Base: Type u
+  Fiber: X → Type v
 
 class ISetHom (I1 I2: ISet) where
-  f: I1.X → I2.X
-  α (x: I1.X): I1.A x → I2.A (f x)   
+  basemap: I1.Base → I2.Base
+  fibermap (x: I1.Base): I1.Fiber x → I2.Fiber (basemap x)
 
 def ISetId (I: ISet): ISetHom I I := {
-  f := id
-  α := fun _ a => a 
+  basemap := id
+  fibermap := fun _ a => a 
 }
 
 def ISetComp {I1 I2 I3: ISet} (h1: ISetHom I1 I2) (h2: ISetHom I2 I3): ISetHom I1 I3 := {
-  f := h2.f ∘ h1.f
-  α := fun x a => h2.α (h1.f x) (h1.α x a)
+  basemap := h2.basemap ∘ h1.basemap
+  fibermap := fun x a => h2.fibermap (h1.basemap x) (h1.fibermap x a)
 }
 
 instance: Category ISet := {
@@ -38,8 +40,34 @@ instance: Limits.HasColimits ISet := by
   -- given functor `F : J ⥤ ISet` show that `Limits.HasColimit F`
   sorry
 
--- equivalence between ISet and Arrow(Set)
-def eqv: CategoryTheory.Equivalence ISet (Arrow (Type u)) := sorry
+-- given an indexed set I, DPair I is the type of dependent pairs (x: X, a: Fiber x)
+class DPair (I: ISet) where
+  x: I.Base
+  a: I.Fiber x
+
+-- given a morphism of indexed sets there is a corresponding map between dependent pairs
+def DPairMap {I1 I2: ISet} (f: ISetHom I1 I2): DPair I1 → DPair I2 :=
+  fun p => {
+    x := f.basemap p.x
+    a := f.fibermap p.x p.a
+  }
+
+-- Grothendeick functor from ISet to Arrow(Set)
+def GrothFunc: Functor ISet (Arrow (Type u)) := {
+  obj := fun I: ISet => Arrow.mk (fun p: DPair I => p.x)
+  map := fun f => {
+    left := DPairMap f
+    right := f.basemap
+  }
+}
+
+-- Equivalence between ISet and Arrow(Set)
+def eqv: CategoryTheory.Equivalence ISet (Arrow (Type u)) := {
+  functor := GrothFunc
+  inverse := sorry
+  unitIso := sorry
+  counitIso := sorry
+}
 
 -- given category T lift to functor from [T, ISet] to [T, Arrow(Set)] via the equivalence
 def eqv_comp (T: Type u1) [Category T]: Functor (T ⥤ ISet) (T ⥤ (Arrow (Type u2))) := {
@@ -51,8 +79,8 @@ def eqv_comp (T: Type u1) [Category T]: Functor (T ⥤ ISet) (T ⥤ (Arrow (Type
 instance: Limits.HasColimits (Arrow (Type u)) := CategoryTheory.Arrow.hasColimits
 
 -- if f: T ⥤ T' is a functor between small categories and C is cocomplete then every functor F: T ⥤ C has a left kan extension along f
-instance {T: Type u1} {T': Type u2} {C: Type u3} [SmallCategory T] [SmallCategory T'] [Category C] [Limits.HasColimits C] {f: Functor T T'}: ∀ F: Functor T C, f.HasLeftKanExtension F := sorry
+instance {T T' C: Type*} [SmallCategory T] [SmallCategory T'] [Category C] [Limits.HasColimits C] {f: Functor T T'}: ∀ F: Functor T C, f.HasLeftKanExtension F := sorry
 
-theorem main {T: Type u1} {T': Type u2} [SmallCategory T] [SmallCategory T'] (f: Functor T T'):
+theorem main {T T': Type*} [SmallCategory T] [SmallCategory T'] (f: Functor T T'):
   IsIsomorphic (Functor.lan f ⋙ eqv_comp T') (eqv_comp T ⋙ Functor.lan f) -- isomorphism in the functor category
   := by sorry
